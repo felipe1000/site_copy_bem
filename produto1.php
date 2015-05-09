ddda228f043c8b13e64be68b91818cfee1a145c2
Felipe Guedes Maciel <felipeguedes01@hotmail.com>
2015-05-09 00:19:36 -0300

    Primeiro Commit dos arquivos do Site

---
+++ produto1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:::Copybem:::</title>
+<link href="divs.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+   <div id="container">
+      <div id="cabecalho"><img src="imagens/logo.png" width="223" height="82" /></div>
+      <div id="meio">
+         <div id="menu"><?php include ("menu.html");?><div id="propaganda">Content for  id "propaganda" Goes Here</div></div>
+         <div id="loja">
+            <div id="vitrine">
+              <div id="produto"><?php include ("produto1.html");?></div>
+              <div id="comprar"><?php include ("comprar1.html");?></div>
+            </div>
+         </div>
+      <div id="rodape">&nbsp;Av.Ceará, 711 Porto Alegre/RS <br />&nbsp;Copybem - Usinagens</div>
+   </div>
+</body>
+</html>
